d4f74fa309588dfce16d48463e595707267337cb
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-01 18:44:18 -0300

    Add basic operation functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,39 @@
 console.log("Welcome");
 
-// Really wanted to make the calculator draggable, so here it WILL be VV
-
+// Really wanted to make the calculator draggable
 $( ".calculator-chassis" ).draggable();
+
+let userFirstNumber = 0;
+let userOperator = "+";
+let userSecondNumber = 0;
+
+function operate(operator, firstNumber, secondNumber) {
+    if (operator == '+') {
+        calculatorAdd(firstNumber, secondNumber);
+    }
+    else if (operator == '-') {
+        calculatorSubtract(firstNumber, secondNumber);
+    }
+    else if (operator == '*') {
+        calculatorSubtract(firstNumber, secondNumber);
+    }
+    else {
+        calculatorDivide(firstNumber,secondNumber);
+    }
+}
+
+
+function calculatorAdd(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+function calculatorSubtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function calculatorMultiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+function calculatorDivide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
