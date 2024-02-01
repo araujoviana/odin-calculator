abc8e1545e95561a6b563acc78a4305b326e13e8
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-01 20:00:01 -0300

    Add basic two number operations to script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,22 +3,58 @@ console.log("Welcome");
 // Really wanted to make the calculator draggable
 $( ".calculator-chassis" ).draggable();
 
+calculatorDisplay = document.querySelector(".calculator-display");
+
 let userFirstNumber = 0;
 let userOperator = "+";
 let userSecondNumber = 0;
+let insertingSecondNumber = false;
+
+let numberButtons = document.querySelectorAll(".calculator-number");
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', () => {
+        calculatorDisplay.textContent += numberButton.textContent;
+    })
+});
+
+let operatorButtons = document.querySelectorAll(".calculator-operator");
+operatorButtons.forEach(operatorButton => {
+
+    operatorButton.addEventListener('click', () => {
+
+        userOperator = operatorButton.textContent;
+        userFirstNumber = parseInt(calculatorDisplay.textContent);
+        calculatorDisplay.textContent = '';
+        insertingSecondNumber = true;
+
+    })
+
+});
+
+let equalButton = document.querySelector(".calculator-equal");
+equalButton.addEventListener('click', () => {
+
+    userSecondNumber = parseInt(calculatorDisplay.textContent);
+    calculatorDisplay.textContent = '';
+    calculatorDisplay.textContent = operate(userOperator, userFirstNumber, userSecondNumber);
+
+})
+
+
+
 
 function operate(operator, firstNumber, secondNumber) {
     if (operator == '+') {
-        calculatorAdd(firstNumber, secondNumber);
+        return calculatorAdd(firstNumber, secondNumber);
     }
     else if (operator == '-') {
-        calculatorSubtract(firstNumber, secondNumber);
+        return calculatorSubtract(firstNumber, secondNumber);
     }
     else if (operator == '*') {
-        calculatorSubtract(firstNumber, secondNumber);
+        return calculatorSubtract(firstNumber, secondNumber);
     }
     else {
-        calculatorDivide(firstNumber,secondNumber);
+        return calculatorDivide(firstNumber,secondNumber);
     }
 }
 
