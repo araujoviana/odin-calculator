249c7dc7c79b019a7ba049285656865ae539b518
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-01 23:02:40 -0300

    Add zero division error

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,68 +1,54 @@
-console.log("Welcome");
-
 // Really wanted to make the calculator draggable
 $( ".calculator-chassis" ).draggable();
 
-calculatorDisplay = document.querySelector(".calculator-display");
+let calculatorDisplay = document.querySelector(".calculator-display");
 
-const numberArray = [];
-const operatorArray = [];
+let currentNumber = '';
+let currentResult = 0;
+let currentOperator = null;
 
 let numberButtons = document.querySelectorAll(".calculator-number");
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', () => {
-        calculatorDisplay.textContent += numberButton.textContent;
+        currentNumber += numberButton.textContent;
+        calculatorDisplay.textContent = currentNumber;
     })
 });
 
 let clearButton = document.querySelector(".calculator-clear");
 clearButton.addEventListener('click', () => {
     calculatorDisplay.textContent = '';
-    numberArray.length = 0;
-    operatorArray.length = 0;
+    currentNumber = '';
+    currentOperator = null;
 })
 
 let operatorButtons = document.querySelectorAll(".calculator-operator");
 operatorButtons.forEach(operatorButton => {
-
     operatorButton.addEventListener('click', () => {
-
-        if (!(isNaN(parseInt(calculatorDisplay.textContent)))) {
-            operatorArray.push(operatorButton.textContent);  
-            numberArray.push(parseInt(calculatorDisplay.textContent));
-            calculatorDisplay.textContent = '';
+        if (currentNumber !== '') {
+            if (currentOperator !== null) {
+                let operationResult = operate(currentOperator, currentResult, parseInt(currentNumber));
+                currentResult = typeof operationResult === "number" ? Math.round(operationResult) : operationResult;
+                calculatorDisplay.textContent = currentResult;
+            } else {
+                currentResult = parseInt(currentNumber);
+            }
+            currentOperator = operatorButton.textContent;
+            currentNumber = '';
         }
-        
     })
-
 });
 
 let equalButton = document.querySelector(".calculator-equal");
 equalButton.addEventListener('click', () => {
-
-    if (!(isNaN(parseInt(calculatorDisplay.textContent)))) {
-        numberArray.push(parseInt(calculatorDisplay.textContent));
+    if (currentNumber !== '' && currentOperator !== null) {
+        let operationResult = operate(currentOperator, currentResult, parseInt(currentNumber));
+        currentResult = typeof operationResult === "number" ? Math.round(operationResult) : operationResult;
+        calculatorDisplay.textContent = currentResult;
+        currentOperator = null;
+        currentNumber = '';
     }
-    calculatorDisplay.textContent = '';
-    calculatorDisplay.textContent = parseOperations(numberArray, operatorArray);
-    numberArray.length = 0;
-    operatorArray.length = 0;
-})
-
-function parseOperations(numbers, operators) {
-    let result = numbers[0];
-    for (let i = 0; i < operators.length; i++) {
-        let operator = operators[i];
-        let secondNumber = numbers[i+1];
-        result = operate(operator, result, secondNumber);
-        console.log(secondNumber + ' ' + operator + ' ' + result);  
-    }
-    console.table(numbers);
-    console.table(operators);
-    console.log("FINAL " + ' ' + result);  
-    return result;
-}
-
+});
 
 function operate(operator, firstNumber, secondNumber) {
     if (operator == '+') {
@@ -79,18 +65,19 @@ function operate(operator, firstNumber, secondNumber) {
     }
 }
 
-
 function calculatorAdd(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
 function calculatorSubtract(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
 }
-
 function calculatorMultiply(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
 function calculatorDivide(firstNumber, secondNumber) {
+    if (secondNumber <= 0 || firstNumber <= 0) {
+        // idk how to write snarky comments so i think this is good enough
+        return "why? :-(";
+    }
     return firstNumber / secondNumber;
 }
-
