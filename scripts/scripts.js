bd9255eaf494e418e0285d3204f52842645fbba1
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-01 22:15:17 -0300

    Fixed multiple number operations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,6 +15,13 @@ numberButtons.forEach(numberButton => {
     })
 });
 
+let clearButton = document.querySelector(".calculator-clear");
+clearButton.addEventListener('click', () => {
+    calculatorDisplay.textContent = '';
+    numberArray.length = 0;
+    operatorArray.length = 0;
+})
+
 let operatorButtons = document.querySelectorAll(".calculator-operator");
 operatorButtons.forEach(operatorButton => {
 
@@ -38,26 +45,21 @@ equalButton.addEventListener('click', () => {
     }
     calculatorDisplay.textContent = '';
     calculatorDisplay.textContent = parseOperations(numberArray, operatorArray);
-
+    numberArray.length = 0;
+    operatorArray.length = 0;
 })
 
 function parseOperations(numbers, operators) {
-    // returns nothing if the operation is invalid EDGE CASES CHECK LATER
-    let result = 0;
-    for (let i = 0; i <= numbers.length-1; i++) {
-        let firstNumber = parseInt(numbers[i]);
-        let secondNumber = parseInt(numbers[i+1]);
+    let result = numbers[0];
+    for (let i = 0; i < operators.length; i++) {
         let operator = operators[i];
-
-        if (secondNumber !== undefined && operator !== undefined) {
-            let operationResult = operate(operator, firstNumber, secondNumber);
-            if (typeof operationResult == "number") {
-                result += operationResult;
-            }
-        }
-        
-        console.log(firstNumber + ' ' + secondNumber + ' ' + operator + ' ' + result);
+        let secondNumber = numbers[i+1];
+        result = operate(operator, result, secondNumber);
+        console.log(secondNumber + ' ' + operator + ' ' + result);  
     }
+    console.table(numbers);
+    console.table(operators);
+    console.log("FINAL " + ' ' + result);  
     return result;
 }
 
@@ -70,7 +72,7 @@ function operate(operator, firstNumber, secondNumber) {
         return calculatorSubtract(firstNumber, secondNumber);
     }
     else if (operator == '*') {
-        return calculatorSubtract(firstNumber, secondNumber);
+        return calculatorMultiply(firstNumber, secondNumber);
     }
     else {
         return calculatorDivide(firstNumber,secondNumber);
